4b7dd9576c2b26db1acd4d980930ae4ec4004eb2
Denis Krivoshspko <mrfisher123@mail.ru>
2018-04-18 17:08:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import time
 
 
 def update():
+    time.sleep(5)
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute('SELECT tg_id FROM users')
     for tg_id in c.fetchall():
         tg.send_wall(tg_id[0])
-    time.sleep(1)
 
 
 
